f6107167383ca6e41f8fbc06186fe216ee4aba41
MohanDeredla <mohanreddyderedla148@gmail.com>
2025-08-02 11:39:58 +0530

    Project Initialisation

---
+++ workforce-mgmt/src/main/java/com/railse/hiring/workforcemgmt/WorkforceManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.railse.hiring.workforcemgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkforceManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkforceManagementApiApplication.class, args);
+	}
+
+}
